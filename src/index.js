ae8421bcc383dc28f7f9e1a6ae9d35983d8e09fe
Mira Jones <meschoolstuffya@gmail.com>
2018-04-16 10:50:59 -0600

    Starting the reducer

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import reset from "./"
+import reset from "./";
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+// import registerServiceWorker from "./registerServiceWorker";
 import { HashRouter } from "react-router-dom";
-// import Provider from "react";
-import store from "./store"
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <HashRouter>
       <App />
     </HashRouter>
-  // </Provider>
-  ,document.getElementById("root")
+  </Provider>,
+  document.getElementById("root")
 );
-registerServiceWorker();
+// registerServiceWorker();
---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+var initialState = {
+    userid : 0,
+    userName : "",
+    userUserName : "",
+    orderId : 0,
+    products : [],
+    total: 0
+}
+
+export default function reducer(state = initialState) {
+    switch(action.payload) {
+        default:
+        return state
+    }
+}
